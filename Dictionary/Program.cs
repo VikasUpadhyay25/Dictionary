92f227e1ef3f0074f0535fbbe22746828e1d104d
Administrator <Administrator@CHAUHAN>
2022-03-14 13:16:58 +0530

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            names.Add("3", "Vikas");
+            names.Add("1", "Upadhyay");
+            names.Add("2", "Kumar");
+
+            foreach(KeyValuePair<string, string>kv in names)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
